10de0ca87d4b9ac34d389550e691eb68666ed6ab
Alina Mikheeva <alinka.mih04@gmail.com>
2026-05-01 00:50:53 +0300

    lalala

--- examples/conflict_case/base/schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE users (
-    id INT PRIMARY KEY,
-    email VARCHAR(255),
-    name VARCHAR(255)
-);
--- examples/conflict_case/branch_b/schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE users (
-    id INT PRIMARY KEY,
-    email VARCHAR(255) UNIQUE,
-    name VARCHAR(255)
-);
